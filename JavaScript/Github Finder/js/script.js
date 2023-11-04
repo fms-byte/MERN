e9674ac9cb0d7e9f88aa24410f5e018db9ae7223
Farhan-FMS <b190305043@cse.jnu.ac.bd>
2023-11-04 12:25:59 +0600

    github finder

---
+++ JavaScript/Github Finder/js/script.js
@@ -0,0 +1,25 @@
+let searchBtn = document.querySelector('#searchBtn');
+let searchUser = document.querySelector('#searchUser');
+let ui = new UI();
+
+searchBtn.addEventListener('click', (e) => {
+    let userText = searchUser.value;
+    if(userText != ''){
+        //fetch api
+        fetch(`https://api.github.com/users/${userText}`)
+            .then(result => result.json())
+            .then(data => {
+                //console.log(data);
+                if(data.message == 'Not Found'){
+                    //show alert
+                    ui.showAlert('User not found', 'alert alert-danger');
+                }else{
+                    //show profile
+                    ui.showProfile(data);
+                }
+            })
+    } else {
+        //clear profile
+        ui.clearProfile();
+    }
+});
