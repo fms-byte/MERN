6766443743dd7eac7996c420030a57b17b8a1927
fms-byte <fsh69711@gmail.com>
2023-11-18 14:05:40 +0600

    added Navigate to Home

--- React Redux/e-restaurent/src/components/body/BodyComponent.js
+++ React Redux/e-restaurent/src/components/body/BodyComponent.js
@@ -1,20 +1,21 @@
-import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Menu from "./Menu";
 import Contact from "./Contact";
 import About from "./About";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 const BodyComponent = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={ <Home />}/>
-        <Route path="/menu" element={ <Menu />}/>
-        <Route path="/contact" element={ <Contact />}/>
-        <Route path="/about" element={ <About />}/>
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default BodyComponent;
